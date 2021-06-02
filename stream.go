6706bde977ce9a6828cf66679bfde7efec1bb477
DeedleFake <deedlefake@users.noreply.github.com>
2021-06-02 13:50:54 -0400

    fquic: implement `Stream`

--- stream.go
+++ stream.go
@@ -1,10 +1,16 @@
 package fquic
 
 import (
+	"errors"
 	"net"
 	"time"
 
 	"github.com/lucas-clemente/quic-go"
+)
+
+var (
+	ErrWriteOnly = errors.New("write-only stream")
+	ErrReadOnly  = errors.New("read-only stream")
 )
 
 type Stream struct {
@@ -25,24 +31,48 @@ func (s *Stream) Conn() *Conn {
 	return s.conn
 }
 
-func (s *Stream) CanSend() bool {
-	return s.s != nil
+func (s *Stream) Stream() quic.Stream {
+	qs, _ := s.s.(quic.Stream)
+	return qs
+}
+
+func (s *Stream) ReceiveStream() quic.ReceiveStream {
+	return s.r
+}
+
+func (s *Stream) SendStream() quic.SendStream {
+	return s.s
 }
 
 func (s *Stream) CanReceive() bool {
 	return s.r != nil
 }
 
+func (s *Stream) CanSend() bool {
+	return s.s != nil
+}
+
 func (s *Stream) Read(buf []byte) (int, error) {
-	panic("Not implemented.")
+	if s.r == nil {
+		return 0, ErrWriteOnly
+	}
+
+	return s.r.Read(buf)
 }
 
 func (s *Stream) Write(data []byte) (int, error) {
-	panic("Not implemented.")
+	if s.s == nil {
+		return 0, ErrReadOnly
+	}
+
+	return s.s.Write(data)
 }
 
 func (s *Stream) Close() error {
-	panic("Not implemented.")
+	if s.s == nil {
+		return nil
+	}
+	return s.s.Close()
 }
 
 func (s *Stream) LocalAddr() net.Addr {
@@ -54,13 +84,29 @@ func (s *Stream) RemoteAddr() net.Addr {
 }
 
 func (s *Stream) SetDeadline(t time.Time) error {
-	panic("Not implemented.")
+	if s.s == nil {
+		return ErrReadOnly
+	}
+	if s.r == nil {
+		return ErrWriteOnly
+	}
+
+	type deadliner interface {
+		SetDeadline(time.Time) error
+	}
+	return s.s.(deadliner).SetDeadline(t)
 }
 
 func (s *Stream) SetReadDeadline(t time.Time) error {
-	panic("Not implemented.")
+	if s.r == nil {
+		return ErrWriteOnly
+	}
+	return s.r.SetReadDeadline(t)
 }
 
 func (s *Stream) SetWriteDeadline(t time.Time) error {
-	panic("Not implemented.")
+	if s.s == nil {
+		return ErrReadOnly
+	}
+	return s.s.SetWriteDeadline(t)
 }
