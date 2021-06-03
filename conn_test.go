4638ffe76e0ae4bf1139688881d3cf709c9b6a4f
DeedleFake <deedlefake@users.noreply.github.com>
2021-06-03 01:18:24 -0400

    fquic: add a test that doesn't work

---
+++ conn_test.go
@@ -0,0 +1,101 @@
+package fquic_test
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"io"
+	"net"
+	"sync"
+	"testing"
+
+	"github.com/DeedleFake/fquic"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSimple(t *testing.T) {
+	testData := []byte("some test data that is all lowercase")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	t.Cleanup(cancel)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	addr := make(chan net.Addr, 1)
+	go func() {
+		defer wg.Done()
+
+		lis, err := fquic.Listen("test-simple", "localhost:0")
+		assert.NoError(t, err, "open listener")
+		addr <- lis.Addr()
+		defer func() {
+			err := lis.Close()
+			assert.NoError(t, err, "close listener")
+		}()
+
+		c, err := lis.Accept(ctx)
+		assert.NoError(t, err, "accept connection")
+		defer func() {
+			err := c.Close()
+			assert.NoError(t, err, "close connection")
+		}()
+
+		s, err := c.AcceptStream(ctx)
+		assert.NoError(t, err, "accept stream")
+		defer func() {
+			err := s.Close()
+			assert.NoError(t, err, "close stream")
+		}()
+		assert.True(t, s.CanWrite(), "bidirectional stream")
+
+		var buf [1024]byte
+		n, err := s.Read(buf[:])
+		if err != io.EOF {
+			assert.NoError(t, err, "read")
+		}
+		assert.Equal(t, testData, buf[:n], "data")
+
+		n, err = s.Write(bytes.ToUpper(buf[:n]))
+		assert.NoError(t, err, "write")
+		assert.Equal(t, len(testData), n, "amount written")
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		d := &fquic.Dialer{
+			TLSConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+			Protocol: "test-simple",
+		}
+		c, err := d.Dial((<-addr).String())
+		assert.NoError(t, err, "dial")
+		defer func() {
+			err := c.Close()
+			assert.NoError(t, err, "close connection")
+		}()
+
+		s, err := c.NewStream(false)
+		assert.NoError(t, err, "open stream")
+		defer func() {
+			err := s.Close()
+			assert.NoError(t, err, "close stream")
+		}()
+
+		n, err := s.Write(testData)
+		assert.NoError(t, err, "write")
+		assert.Equal(t, len(testData), n, "amount written")
+
+		var buf [1024]byte
+		n, err = s.Read(buf[:])
+		if err != io.EOF {
+			assert.NoError(t, err, "read")
+		}
+		assert.Equal(t, bytes.ToUpper(testData), buf[:n], "data")
+	}()
+
+	wg.Wait()
+}
--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ require (
 	github.com/lucas-clemente/quic-go v0.21.0
 	github.com/onsi/ginkgo v1.16.3 // indirect
 	github.com/onsi/gomega v1.13.0 // indirect
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
 	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/sys v0.0.0-20210601080250-7ecdf8ef093b // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
